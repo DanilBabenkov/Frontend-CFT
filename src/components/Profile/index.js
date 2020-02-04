82dfe79a64b9bd3be362a99a2bebfe34f8854064
v1teka <v1teka@yandex-team.ru>
2020-02-04 23:56:25 +0700

    Содержимое Profile выделено в отдельный компонент

---
+++ src/components/Profile/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+import {TagInput} from '../TagInput';
+
+function MainInfo() {
+  console.log(TagInput)
+  return (
+    <React.Fragment>
+      <Avatar>АЛ</Avatar>
+      <Typography variant="h6" gutterBottom>
+        Общая информация
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="firstName"
+            name="firstName"
+            label="Имя"
+            fullWidth
+            autoComplete="fname"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Фамилия"
+            fullWidth
+            autoComplete="lname"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="about"
+            name="about"
+            label="О себе"
+            fullWidth
+            multiline
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+class SkillTags extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {checked: false , tags: []}
+        this.onTagsChanged = this.onTagsChanged.bind(this);
+    }
+
+    handleCheckboxChange = event =>
+    this.setState({ checked: event.target.checked })
+
+    onTagsChanged(tags) {
+        this.setState({tags})
+    }
+
+    render() {
+        return (
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox color="secondary" checked={this.state.checked}
+              onChange={this.handleCheckboxChange} name="isTeacher" value="yes" />}
+                label="Я преподаватель"
+              />
+          </Grid>
+          {this.state.checked ? <Grid  item xs={12}>
+            <TagInput
+              tags={this.state.tags} 
+              onTagsChanged={this.onTagsChanged}
+              inputStyle={`
+                background: white;
+                &::-webkit-input-placeholder {
+                  font-weight: inherit;
+                  font-style: inherit;
+                  color:  inherit;
+                  font-size: inherit;
+                }
+              `}
+              tagStyle={`
+                background: #f50057;
+                font-weight: inherit;
+                  font-style: inherit;
+                  font-size: inherit;
+              `}
+            />
+          </Grid> : ''}
+          
+        </Grid>
+        )
+    }
+}
+
+export default {SkillTags, MainInfo};
