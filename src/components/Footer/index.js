89e66ca4ac37016a8b446c431384ee4ace022e04
v1teka <v1teka@yandex-team.ru>
2020-02-04 23:55:28 +0700

    Наполнены Footer Header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'© '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Repetitto
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
 export default function Footer() {
     return (
         <footer>
-            Footer
+            <Copyright />
         </footer>
     )
 }
