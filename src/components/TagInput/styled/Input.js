5f7c53f28a7b73b72aeac630ba877be4d6bf2215
v1teka <v1teka@yandex-team.ru>
2020-02-04 23:56:57 +0700

    Добавлен компонент редактирования тегов

---
+++ src/components/TagInput/styled/Input.js
@@ -0,0 +1,9 @@
+var _templateObject = _taggedTemplateLiteral(['\n  border: none;\n  outline: none;\n  display: inline-block;\n  width: 100%;\n'], ['\n border: none;\n  outline: none;\n  display: inline-block;\n  width: 100%;\n \n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+import styled from 'styled-components';
+
+var Input = styled.input(_templateObject);
+
+export default Input;
---
+++ src/components/TagInput/styled/Wrapper.js
@@ -0,0 +1,9 @@
+
+import styled from 'styled-components';
+var _templateObject = _taggedTemplateLiteral(['\n  padding: 10px;\n   width: 70%;\n'], ['\n   padding: 10px;\n  width: 70%;\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var Wrapper = styled.div(_templateObject);
+
+export default Wrapper;
