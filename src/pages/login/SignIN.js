be74f26c51b73708fe9df017ac4eef45a00ef63c
v1teka <v1teka@yandex-team.ru>
2020-02-05 16:02:24 +0700

    Изменение навигации с использованием Router на страницах авторизации

--- src/pages/login/SignIN.js
+++ src/pages/login/SignIN.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -81,17 +81,19 @@ export default function SignIn() {
             variant="contained"
             color="primary"
             className={classes.submit}
+            component={Link}
+            to="/profile"
           >
             Войти
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link to="#" variant="body2">
                 Забыли пароль?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/sign_up" variant="body2">
+              <Link to="/sign_up" variant="body2">
                 {"Еще нет аккаунта? Зарегистрироваться"}
               </Link>
             </Grid>
--- src/pages/login/SignUP.js
+++ src/pages/login/SignUP.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -113,8 +113,9 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/sign_in" variant="body2">
-                Уже есть аккаунт? Войти
+              <span> Уже есть аккаунт? </span>
+              <Link to="/sign_in" variant="body2">
+                Войти
               </Link>
             </Grid>
           </Grid>
