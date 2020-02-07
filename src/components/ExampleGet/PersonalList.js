06d34aac3f2429d592613701fa851240ae0b340b
v1teka <v1teka@yandex-team.ru>
2020-02-07 16:42:54 +0700

    Вывод about в список пользователей

--- src/components/ExampleGet/PersonalList.js
+++ src/components/ExampleGet/PersonalList.js
@@ -5,153 +5,164 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grow } from '@material-ui/core';
 const drawerWidth = 240;
 
 const classes = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2),
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  root: {
+    display: 'justify-content',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
     },
-    root: {
-      display: 'justify-content',
-    },
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: 36,
-    },
-    hide: {
-      display: 'none',
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-      whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    drawerClose: {
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      overflowX: 'hidden',
-      width: theme.spacing(7) + 1,
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9) + 1,
-      },
-    },
-    toolbar: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      padding: theme.spacing(0, 1),
-      ...theme.mixins.toolbar,
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
-  }));
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 class PersonsList extends React.Component {
-    
-    state = {
-        persons: []
-    }
 
-    componentDidMount() {
-        axios.get('http://repetito.herokuapp.com/user')
-        .then(res => {
-            console.log(res);
-            this.setState({ persons: res.data});
-        })
-    }
+  state = {
+    persons: []
+  }
 
-    render() {
-        return (
-            // <ul>
-            //     {this.state.persons.map(person => <li key={person.id}>{person.name}</li>)}
-            // </ul>
+  componentDidMount() {
+    axios.get('http://repetito.herokuapp.com/user')
+      .then(res => {
+        console.log(res);
+        this.setState({ persons: res.data });
+      })
+  }
 
-            this.state.persons.map(person => (
-                <Grid item key={person.id} xs={12} sm={6} md={4}>
-                  <Card className={classes.card}>
-                    <CardMedia
-                      className={classes.cardMedia}
-                      image="https://source.unsplash.com/random"
-                      title="Image title"
-                    />
-                    <CardContent className={classes.cardContent}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                      {person.firstName} <br/>
-                      {person.lastName}
-                      </Typography>
-                      <Typography>
-                        {'Средняя оценка: '+person.avgMark+'\n'}
-                      </Typography>
-                      <Typography>
-                        {'Цена занятия: '+person.price+'\n'}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button component={Link} to="/profile" size="small" color="primary">
-                      Смотреть
+  render() {
+    return (
+      // <ul>
+      //     {this.state.persons.map(person => <li key={person.id}>{person.name}</li>)}
+      // </ul>
+
+      this.state.persons.map(person => (
+        <Grid item key={person.id} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cardMedia}
+              image="https://source.unsplash.com/random"
+              title="Image title"
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {person.firstName} <br />
+                {person.lastName}
+              </Typography>
+              <Typography>
+                {person.about}
+              </Typography>
+              <Typography>
+                {'Средняя оценка: ' + person.avgMark}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="flex-start"
+              >
+                <Button component={Link} to="/profile" size="small" color="primary">
+                  Смотреть
                       </Button>
-                      
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))
-        )
-    }
+
+                <Button size="small" color="primary">
+                  {person.price + ' руб./занятие'}
+                </Button>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))
+    )
+  }
 }
 
 export default PersonsList;
