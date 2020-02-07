55dc5dccea662cf9ce0c969bba5f432523771d5b
v1teka <v1teka@yandex-team.ru>
2020-02-08 01:21:36 +0700

    Отображение header и footer  только после авторизации

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 
-export default function Layout({children}) {
-    return (
-        <div className="App">
-            <Header></Header>
-            {children}
-            <Footer></Footer>
-        </div>
-    )
+export default function Layout({ children }) {
+    const token = localStorage.getItem('token');
+    const user_id = localStorage.getItem('user_id');
+    const authorised = Boolean(token && token.length && user_id && user_id.length);
+
+    if (authorised)
+        return (
+            <div className="App">
+                <Header></Header>
+                {children}
+                <Footer></Footer>
+            </div>
+        )
+    else
+        return (
+            <div className="App">
+                {children}
+            </div>
+        )
 }
